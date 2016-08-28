b30da65169d7e68d3f3807fb4e94f65161eb8c73
Aaron Calander <aaroncalander@yahoo.com>
2016-08-27 22:19:31 -0700

    Complete 4.4 assignment

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,24 +1,28 @@
-puts "What is your name?"
-  name = gets.chomp
-puts "How old are you?"
-  age = gets.chomp.to_i
-puts "What year were you born"
-  year = gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-  garlic = gets.chomp
-puts "Would you like to enroll in the company's health insurance?"
-  insurance = gets.chomp
+puts 'How many employees will be processed?'
+  employees = gets.chomp.to_i
 
-if garlic == "Yes"
-  garlic_result = true
-else
-  garlic_result = false
+loop do
+  puts 'What is your name?'
+    name = gets.chomp
+  puts 'How old are you?'
+    age = gets.chomp.to_i
+  puts 'What year were you born?'
+    year = gets.chomp.to_i
+  puts 'Our company cafeteria serves garlic bread. Should we order some for you?'
+    garlic = gets.chomp
+  puts "Would you like to enroll in the company's health insurance?"
+    insurance = gets.chomp
+  puts "Name your allergies. Enter them one at a time and type 'done' when finished."
+    allergies = gets.chomp
+    
+until allergies == "sunshine" || allergies == "done"
+  puts "Name your allergies. Enter them one at a time and type 'done' when finished."
+    allergies = gets.chomp
 end
-
-if insurance == "Yes"
-  insurance_result = true
-else
-  insurance_result = false
+  
+if allergies == "sunshine"
+  puts "Probably a vampire."
+break
 end
 
 if year + age == 2016
@@ -27,22 +31,39 @@ else
   correct_age = false
 end
 
-if (correct_age && (garlic_result || insurance_result)) == true 
+if correct_age && (garlic == 'Yes' || insurance == 'Yes')
   result = 'Probably not a vampire.'
-end
-
-if (correct_age && (garlic_result || insurance_result)) == false
+elsif correct_age && (garlic == 'Yes' || insurance == 'Yes')
   result = 'Probably a vampire.'
-end
-
-if (correct_age && (garlic_result && insurance_result)) == false
-  result = "Almost certainly a vampire."
-end
-
-if name == "Drake Cula" || name == "Tu Fang"
+elsif correct_age || (garlic == 'No' && insurance == 'No')
+  result = 'Almost certainly a vampire.'
+elsif name == 'Drake Cula' || name == 'Tu Fang'
   result = 'Definitely a vampire.'
 else
   result = 'Results inconclusive.'
 end
 
 p result
+employees -= 1
+
+if employees == 0
+    break
+  end
+end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+elsif correct_age || (garlic == 'No' && insurance == 'No')
+  result = 'Almost certainly a vampire.'
+elsif name == 'Drake Cula' || name == 'Tu Fang'
+  result = 'Definitely a vampire.'
+else
+  result = 'Results inconclusive.'
+end
+
+p result
+number_employees -= 1
+
+if number_employees == 0
+    break
+  end
+end
