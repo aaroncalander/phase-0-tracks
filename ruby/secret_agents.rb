faa4554c346875464247b90f59bf8fe589430be5
Aaron Calander <aaroncalander@yahoo.com>
2016-08-28 23:59:24 -0700

    Complete 4.6 challenge

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -54,12 +54,42 @@ def decrypt(y)
 	p output
 end
 
-# Driver Code:
-
 # encrypt("abc") 
 # encrypt("zed") 
 # decrypt("bcd") 
 # decrypt("afe")
 # decrypt(encrypt("swordfish"))
 
-# The nested method call works because the encrypt method returns a valid string that can be used as an argument for the decrypt method.
+# The nested method call works because the encrypt method
+# returns a valid string that can be used as an argument 
+# for the decrypt method. Furthermore, because the decrypt 
+# method was defined outside of the encrypt method both 
+# methods could be run within the same line of code.
+
+# Driver Code
+# Ask user if they want to encrypt or decypt
+# Ask user for input (password)
+# Need conditional based on input
+# if encrypt, print output of encrypt method on user's input
+# if decrypt, print output of decrypt method on user's input
+
+validresponse = false
+until validresponse == true
+puts "Welcome, agent."
+puts "Would you like to encrypt or decrypt a password?"
+puts "Enter 1 to encrypt or 2 to decrypt."
+action = gets.chomp
+	if action == "1"
+		puts "Please enter the password to encrypt."
+		password = gets.chomp
+		encrypt(password)
+		validresponse = true
+	elsif action == "2"
+		puts "Please enter the password to decrypt."
+		password = gets.chomp
+		decrypt(password)
+		validresponse = true
+	else
+		puts "That response was invalid. Please try again."
+	end
+end
