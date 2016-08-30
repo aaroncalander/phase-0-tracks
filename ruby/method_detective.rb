19de240f4e5a42706b7dac810c4e7efe93eb976a
Aaron Calander <aaroncalander@yahoo.com>
2016-08-29 23:08:02 -0700

    Complete 5.1 Challenge

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,50 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn.swapcase"
+=> “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".gsub /..?/, &:capitalize
+=> “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
+"zom".insert(1, "o")
+=> “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+"enhance".center(16)
+=> "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"Stop! You’re under arrest!".upcase
+=> "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+a = "the usual "
+a << "suspects" # << = .concat
+=> "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+a = "the usual"
+a.replace "the usual suspects"
+=> "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+a = " suspects"
+a.prepend("the usual")
+=> "the usual suspects"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+a = " suspects"
+a.replace "the usual suspects"
+=> "the usual suspects"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+"The case of the disappearing last letter".chop
+=> "The case of the disappearing last lette"
+# .replace can be used
 
-# "z".<???>
-# => 122 
+"The mystery of the missing first letter".delete "T"
+=> "he mystery of the missing first letter"
+# .replace can be used
+
+"Elementary,    my   dear        Watson!".squeeze (" ")
+=> "Elementary, my dear Watson!"
+
+# "z".ord
+=> 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+"How many times does the letter 'a' appear in this string?".count "a"
+=> 4
