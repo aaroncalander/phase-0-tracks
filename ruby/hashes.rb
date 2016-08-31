ed8c5af8ea19c9c15551094bf6beb5cf4f848e99
Aaron Calander <aaroncalander@yahoo.com>
2016-08-30 21:18:58 -0700

    Complete 5.2 challenge

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,11 +1,12 @@
-# Ask user for client detail input:
+# Ask user questions to get client details:
 # client's first name, last name, age, number of children, favorite color, square
-# footage, decor theme, budget, has money(true or false)
-# Create hash with keys/values to store client information.
-# Convert input to correct data type.
-# Print hash when all questions have been answered.
-# Allow user to update a key if entered incorrectly:
-# use .to_sym method to allow this.
+# footage, decor theme, budget
+# Create hash with keys/values to store survey information.
+# Convert input values to correct data type using built in String methods.
+# Print hash when all questions have been answered and all keys have a value.
+# Allow user to update the value of a key if entered incorrectly:
+# Re-enter key and be prompted to enter new value.
+# If user enters 'none' exit program.
 # Print updated version of hash.
 # Exit the program.
 
@@ -25,5 +26,28 @@ puts "What is the decor theme?"
   decor_theme = gets.chomp
 puts "What is the budget for the project?"
   budget = gets.chomp.to_i
-puts "Is the client ready to deposit money?"
-  money = gets.chomp.to_bool
+
+client_survey = {
+	first_name: first_name,
+	last_name: last_name,
+	age: age,
+	children: children,
+	color: color,
+	square_feet: square_feet,
+	decor_theme: decor_theme,
+	budget: budget
+}
+
+p client_survey
+
+puts "Were any of your answers incorrect (i.e. decor_theme)? If so, please enter the key value now or type 'none' to complete survey."
+  answer = gets.chomp
+
+if answer == "none"
+else
+	puts "Re-enter new #{answer} here:"
+	new_answer = gets.chomp
+	client_survey[answer.to_sym] = new_answer
+end
+
+p client_survey
