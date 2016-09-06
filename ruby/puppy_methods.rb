dff3e577cf677f2e2cc0f60fe8ede10907859358
Aaron Calander <aaroncalander@Aarons-MacBook-Pro.local>
2016-09-06 16:52:18 -0700

    Add driver code and methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,29 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+# Additional methods:
+  def speak(int)
+  	puts 'Woof!' * int
+  end
 
+  def roll_over
+  	puts '*rolls over*'
+  end
+
+  def dog_years(int)
+  	puts int * 7
+  end
+
+  def attack(name)
+  	puts "I would rather just lick #{name}."
+  end
 end
 
+# Driver Code:
+lucky = Puppy.new
+
+lucky.fetch('ball')
+lucky.speak(5)
+lucky.roll_over
+lucky.dog_years(7)
+lucky.attack('Aaron')
