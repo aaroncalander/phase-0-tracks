82994125e9babc0f2cf6e39d3433f259000c1c6e
Aaron Calander <aaroncalander@Aarons-MacBook-Pro.local>
2016-09-08 13:58:30 -0700

    Finish Release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,20 +1,53 @@
 class Santa
-  
+
+  def initialize(gender, ethnicity)
+  	puts "Initializing Santa instance ..."
+  	@gender = gender
+  	@ethnicity = ethnicity
+  	@reindeer_ranking = ['Rudolph', 'Dasher', 'Dancer', 'Prancer', 'Vixen', 'Comet', "Cupid", 'Donner', 'Blitzen']
+    @age = 0
+  end
+
   def speak
   	puts "Ho, ho, ho! Haaaappy holidays!"
   end
 
-  def eat_milk_and_cookies(type)
+  def eat_milk_and_cookies(cookie_type)
   	puts "That was a good #{type}!"
-  end
-
-  def initialize
-  	puts "Initializing Santa instance ..."
   end
 end
 
 # Driver Code:
-bad_santa = Santa.new
+# bad_santa = Santa.new
 
-bad_santa.speak
-bad_santa.eat_milk_and_cookies('Fig Newton')
+# bad_santa.speak
+# bad_santa.eat_milk_and_cookies('Fig Newton')
+
+# Sample initialization code:
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+  
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+  puts "There are now #{santas.length} Santas here."
+end
+
+puts "Interacting with the Santas..."
+santas.each do |santa|
+ santa.speak
+end
+
+
