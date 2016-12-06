70f749c6211ad0d645fa78f276b907baa38a9c17
Aaron Calander <aaroncalander@yahoo.com>
2016-12-06 11:20:40 -0800

    Complete Release 0

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+// Release 0
+
+var colors = ["blue", "red", "green", "yellow"];
+var names = ["Ed", "Chuck", "Harry", "Lucky"];
+
+colors.push("purple");
+names.push("Charlie");
+
+console.log(colors);
+console.log(names);
