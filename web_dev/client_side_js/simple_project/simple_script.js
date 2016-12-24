369043abfd692da4170b29dd792cd3fa9f571ffc
Aaron Calander <aaroncalander@yahoo.com>
2016-12-23 17:52:50 -0800

    Complete Release 2

--- web_dev/client_side_js/simple_project/simple_script.js
+++ web_dev/client_side_js/simple_project/simple_script.js
@@ -1,5 +1,8 @@
+// Release 1
+
 console.log("The script is running!");
 
+// Example 1
 var new_item = document.createElement("li");
 var node = document.createTextNode("This is a new line item!");
 new_item.appendChild(node);
@@ -7,5 +10,23 @@ new_item.appendChild(node);
 var element = document.getElementById("ul");
 element.appendChild(new_item);
 
+// Example 2
+var h1 = document.getElementById("h1").style.color = "#B011F5";
+
+// Release 2 - Event Listeners
+
+function addColor(event) {
+  event.target.style.backgroundColor = "#3592C9";
+}
+
 var olist = document.getElementById("ol");
-olist.style.backgroundColor = "#3592C9";
+olist.addEventListener("click", addColor);
+
+function zoom(event) {
+  event.target.style.zoom = "150%";
+}
+
+var webpage = document.getElementById("html")
+webpage.addEventListener("dblclick", zoom);
+
+
